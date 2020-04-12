b16cb7e2fd155a67367fbddf5920ba508c6ea050
Passakorn <33002818+PassakornGearr@users.noreply.github.com>
2020-04-12 17:15:11 +0700

    update final project

--- src/app/pages/login-register/login-register.component.ts
+++ src/app/pages/login-register/login-register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login-register',
   templateUrl: './login-register.component.html',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginRegisterComponent implements OnInit {
 
-  constructor() { }
+    //One way binding
+    message:string = "Hello Angular";
+    //object One way binding
+    profile = {
+      "name":"passakorn",
+      "tel":"0648649570",
+      "gender":"Male"
+    }
+
+    // Two way binding
+    userData = {
+      "email":"",
+      "password":""
+    }
+
+  constructor(private route:Router) { }
 
   ngOnInit(): void {
+  }
+
+  submitLogin() {
+    if(this.userData.email == "admin@email.com" && this.userData.password == "1234" ) { 
+      this.route.navigate(['backend']);
+    }else {
+      alert("login Fail !!!");
+    }
+    // alert("Hey");
   }
 
 }
